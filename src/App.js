73dcb1ee5781162ee369ea711fc1228d71b62e55
unknown <e.fraffo@gmail.com>
2020-04-16 21:23:16 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FormLogin from './components/FormLogin';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+
+          </div>
+          <div className="col-4">
+            <FormLogin/>
+          </div>
+          <div className="col-4">
+
+          </div>
+
+        </div>
+      
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FormLogin.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+
+
+
+function FormLogin() {
+
+    const[usuario, setUsuario] = useState({nombre: '', email: '', password: ''});
+
+//   const [nombre, setNombre] = useState();
+//   const [email, setEmail] = useState();
+//   const [password, setPassword] = useState();
+
+
+  const handleNombreChange = function(event){
+    setUsuario({...usuario, nombre: event.target.value});
+  }
+
+  const handleEmailChange = function(event){
+    setUsuario({...usuario, email: event.target.value});
+  }
+
+  const handlePasswordChange = function(event){
+    setUsuario({...usuario, password: event.target.value});
+  }
+
+  
+  return (    
+        <form>
+            <div className="form-group">
+            <label for="exampleInputName1">Nombre y Apellido</label>
+            <input type="text" className="form-control" id="exampleInputName1" onChange={handleNombreChange} value={usuario.nombre}/>
+            </div>
+            <div className="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={handleEmailChange} value={usuario.email}/>
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div className="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" onChange={handlePasswordChange} value={usuario.password}/>
+            </div>
+            <div className="form-group form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                <label className="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button id="login" className="btn btn-primary">Login</button>
+        </form>          
+  );
+}
+
+export default FormLogin;
